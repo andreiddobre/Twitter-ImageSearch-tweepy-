1905426ec44482d04b39a0e55381896efc97483c
Andrei Dobre <75906525+andreiddobre@users.noreply.github.com>
2023-04-28 01:53:07 +0300

    Update py_tw_image.search.py

--- py_tw_image.search.py
+++ py_tw_image.search.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import tweepy
-import os
 
 # uplifting message
 print("You got this!")
@@ -8,14 +7,13 @@ print("You got this!")
 app = Flask(__name__)
 
 # set up Twitter API credentials
-API_key = "GhBNuW6jZ6ZBuYhE6IKAxQ8k6"
-API_secret = "RgbR1K2tt0BQdp0KoALjvk3u1tG9yhdnymlos1D2wsT4r0PNlp"
-access_token = "571966749-PldY6PPm43TJDo00oZEE69fI7WK5AL3HigSgqWah"
-access_token_secret = "ePxvcxTvBcjqMvYXorEDaxvSLi3LzYnxh2i5v54Ptf8v3"
+API_key = "uILqR7AKhdlBmtzsb1yIgnxrI"
+API_secret = "jAxcutigaWAFFtLSW37Uoi4czE8TYA1LPWkc7OW3Is0i91593y"
+access_token = "919432051-pI7Uvsy9p3DZdC2Z36S5s30bLIyrMyd6PWuMZuIe"
+access_token_secret = "VyD3vu0eST6V5NLfhLyhO0KtVI1e4ozQZYGzi9FmtiMcz"
 
 # authenticate with Twitter API (setup the authentication handler and passing in all necessary access credentials)
-auth = tweepy.OAuthHandler(API_key, API_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuth1UserHandler(API_key, API_secret, access_token, access_token_secret)
 
 # create tweepy API instance (so we can access the Twitter API with the provided authentication credentials)
 api = tweepy.API(auth)
@@ -38,19 +36,8 @@ def index():
         else:
             tweets = api.search_tweets(q=search_term, count=num_tweets, tweet_mode='extended', include_entities=True)
         
-        # store the tweets in a list
-        tweet_texts = []
-        for tweet in tweets:
-            tweet_text = tweet.full_text
-            entities = tweet.entities
-            if entities.get('media'):
-                media_url = entities.get('media')[0]['media_url']
-            else:
-                media_url = None
-            tweet_texts.append({'text': tweet_text, 'media_url': media_url})
-
         # render the template with the tweet texts
-        return render_template('home.html', tweet_texts=tweet_texts, search_term=search_term, only_images=only_images)
+        return render_template('home.html', tweets=tweets, search_term=search_term, only_images=only_images)
     
     # if the request is a GET request, render the home.html template
     return render_template('home.html')
